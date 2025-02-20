032be68caa8070634d9c4388268b735d6e829f72
Lima Neto <limaneto.sist@gmail.com>
2025-02-20 09:38:36 +0200

    Initial

---
+++ cyberboys/app/Http/Controllers/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        // Gerar token
+        $token = $user->createToken('NomeDoApp')->plainTextToken;
+
+        return response()->json(['token' => $token]);
+    }
+}
